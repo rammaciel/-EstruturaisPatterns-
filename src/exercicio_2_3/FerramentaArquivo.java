5eb6b5f6e48fcc9c4edbecfecb3658d08113197a
rammaciel <robertoaugusto00@outlook.com>
2020-06-07 22:12:46 -0300

    Update FerramentaArquivo.java

--- src/exercicio_2_3/FerramentaArquivo.java
+++ src/exercicio_2_3/FerramentaArquivo.java
@@ -8,14 +8,14 @@ public class FerramentaArquivo extends Ferramenta {
 	private File arquivoLog;
 
 	public FerramentaArquivo() {
-		String pacote = "D:\\Faculdade\\Arquitetura Desenvolvimento Sistema\\RepositorioGit\\Aula07\\ccp_aula07\\src\\exercicio_2_3\\";
+		String pacote = "D:\\USJT\\ccp3_patterns\\src\\exercicio_2_2\\publico.txt";
 		arquivoLog = new File(pacote, "log.txt");
 
 		try {
 			if (!arquivoLog.exists())
 				arquivoLog.createNewFile();
 		} catch (IOException e) {
-			System.out.println("Não foi possível criar o arquivo de log:\n" + e);
+			System.out.println("NÃ£o foi possÃ­vel criar o arquivo de log:\n" + e);
 			System.exit(1);
 		}
 	}
@@ -26,7 +26,7 @@ public class FerramentaArquivo extends Ferramenta {
 			writer.write(mensagem + "\n");
 			writer.close();
 		} catch (IOException e) {
-			System.out.println("Não foi possível escrever no arquivo de log:\n" + e);
+			System.out.println("NÃ£o foi possÃ­vel escrever no arquivo de log:\n" + e);
 			System.exit(1);
 		}
 	}
